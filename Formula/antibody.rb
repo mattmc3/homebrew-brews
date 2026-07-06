acdad47bba928f553069c1aa1586602b64c76370
mattmc3 <mattmc3@gmail.com>
2026-07-05 21:42:49 -0400

    feat(antibody): pin formula to v7.0.0
    Add url and sha256 for the v7.0.0 release tarball. Keep head
    spec for --HEAD installs.

--- Formula/antibody.rb
+++ Formula/antibody.rb
@@ -1,6 +1,8 @@
 class Antibody < Formula
   desc "The fastest shell plugin manager"
   homepage "https://github.com/mattmc3/antibody"
+  url "https://github.com/mattmc3/antibody/archive/refs/tags/v7.0.0.tar.gz"
+  sha256 "7008da0ba5b040abfcd7724882b54174e454f78bbc66cdffe5cea6ccf3e0fb79"
   head "https://github.com/mattmc3/antibody.git", branch: "main"
   license "MIT"
 
